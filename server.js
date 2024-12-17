95e0e781b5686d6e5c145a7edf24aa194732f88e
bang12bang007 <vongoctrungquan2003@gmail.com>
2024-12-17 14:04:56 +0700

    server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS
-app.use(cors());
+app.use(cors()); //Cho phép tất cả các domain truy cập
 app.use(express.json()); // Để server nhận JSON
 
 app.post("/submit", (req, res) => {
