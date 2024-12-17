7a8fba39fe58d6672b358c6235ad3f35b0e653e5
bang12bang007 <vongoctrungquan2003@gmail.com>
2024-12-17 14:11:29 +0700

    p2

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Enable CORS
-app.use(cors()); //Cho phép tất cả các domain truy cập
-app.use(express.json()); // Để server nhận JSON
+app.use(express.json()); // Để nhận dữ liệu JSON
 
-app.post("/submit", (req, res) => {
+// Route xử lý form submit
+app.post('/submit', (req, res) => {
     const data = req.body;
-    console.log("Dữ liệu nhận được:", data);
-    res.status(200).send("Dữ liệu đã được gửi thành công!");
+    console.log('Dữ liệu nhận được:', data);  // In dữ liệu gửi lên console (hoặc có thể lưu vào database)
+    res.status(200).send('Dữ liệu đã được gửi thành công!');
 });
 
+// Khởi động server
 app.listen(port, () => {
     console.log(`Server đang chạy tại http://localhost:${port}`);
 });
